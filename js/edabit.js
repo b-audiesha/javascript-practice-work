42074c7d7d88bfd5807fc548b96897925e46704d
Audiesha Brattin <b.audiesha@gmail.com>
2020-12-16 09:11:59 -0600

    checking git configuration

---
+++ js/edabit.js
@@ -0,0 +1,10 @@
+<!--You are counting points for a basketball game, given the amount of 3-pointers scored and 2-pointers scored, find the final points for the team and return that value.-->
+function points(twoPointers, threePointers) {
+  return (twoPointers * 2) + (threePointers * 3)
+}
+
+/*convert seconds to minutes*/
+function convert(minutes) {
+  return minutes * 60;
+}
+
