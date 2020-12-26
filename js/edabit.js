b48110f2983bd5e6a75d23b366d3b656197fa040
audiesha brattin <a-brattin@gmail.com>
2020-12-25 20:11:12 -0600

    adding new challenges

--- js/edabit.js
+++ js/edabit.js
@@ -49,5 +49,31 @@ function giveMeSomething(a) {
   return "something "+ a;
 }
 
+/*square a number*/
 
+function squared(b) {
+  return b*b;
+}
 
+/*or */
+
+function squared(b) {
+  return b*b;
+}
+/*Create a function that takes a number as an argument, increments the number by +1 and returns the result.*/
+function addition(num) {
+  return num + 1;
+}
+
+/*
+In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. The farmer breeds three species:
+
+  chickens = 2 legs
+cows = 4 legs
+pigs = 4 legs
+The farmer has counted his animals and he gives you a subtotal for each species. You have to implement a function that returns the total number of legs of all the animals.
+*/
+
+function animals(chickens, cows, pigs) {
+  return (chickens * 2) + (cows * 4) +(pigs * 4)
+}
