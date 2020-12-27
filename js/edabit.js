b589f4601696f01fca3b544714381e558b721b88
audiesha brattin <a-brattin@gmail.com>
2020-12-26 22:48:03 -0600

    here for it

--- js/edabit.js
+++ js/edabit.js
@@ -74,6 +74,9 @@ pigs = 4 legs
 The farmer has counted his animals and he gives you a subtotal for each species. You have to implement a function that returns the total number of legs of all the animals.
 */
 
+/*function animals(chickens, cows, pigs) {
+  return (chickens * 2) + (cows * 4) +(pigs * 4)
+}*/
 function animals(chickens, cows, pigs) {
   return (chickens * 2) + (cows * 4) +(pigs * 4)
 }
